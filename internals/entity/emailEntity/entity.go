51c5dd5087b2dae90674b320449b016d2cb01c18
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-12-02 10:09:34 +0100

    Refractor email service to be up and running, also did a refractor in user service when sending an email

--- internals/entity/emailEntity/entity.go
+++ internals/entity/emailEntity/entity.go
@@ -6,3 +6,10 @@ type SendEmailReq struct {
 	EmailSubject string
 	EmailBody    string
 }
+
+type SendBatchEmail struct {
+	Name           string
+	EmailAddresses []string
+	EmailSubject   string
+	EmailBody      string
+}
--- internals/service/emailService/srv.go
+++ internals/service/emailService/srv.go
@@ -3,12 +3,12 @@ package emailService
 import (
 	"fmt"
 	"net/smtp"
-	"os"
 	"test-va/internals/entity/emailEntity"
 )
 
 type EmailService interface {
 	SendMail(req emailEntity.SendEmailReq) error
+	SendBatchEmail(req emailEntity.SendBatchEmail) error
 }
 type emailSrv struct {
 	FromEmail string
@@ -17,11 +17,22 @@ type emailSrv struct {
 	Port      string
 }
 
-func (e emailSrv) SendMail(req emailEntity.SendEmailReq) error {
-	fmt.Println(e.FromEmail, e.Password, e.Host, e.Port)
+func (e emailSrv) SendBatchEmail(req emailEntity.SendBatchEmail) error {
 	auth := smtp.PlainAuth("", e.FromEmail, e.Password, e.Host)
 	addr := e.Host + ":" + e.Port
-	body := []byte(req.EmailSubject + req.EmailBody)
+	header := fmt.Sprintf("From: %v\nTo: %v\n", e.FromEmail, req.EmailAddresses)
+	body := []byte(header + req.EmailSubject + "\n" + req.EmailBody)
+	err := smtp.SendMail(addr, auth, e.FromEmail, req.EmailAddresses, body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (e emailSrv) SendMail(req emailEntity.SendEmailReq) error {
+	auth := smtp.PlainAuth("", e.FromEmail, e.Password, e.Host)
+	addr := e.Host + ":" + e.Port
+	header := fmt.Sprintf("From: %v\nTo: %v\n", e.FromEmail, req.EmailAddress)
+	body := []byte(header + req.EmailSubject + req.EmailBody)
 	err := smtp.SendMail(addr, auth, e.FromEmail, []string{req.EmailAddress}, body)
 	if err != nil {
 		return err
@@ -29,26 +40,26 @@ func (e emailSrv) SendMail(req emailEntity.SendEmailReq) error {
 	return nil
 }
 
+// func SendMail(req emailEntity.SendEmailReq) error {
+// 	// add the email and password below, but remember to remove before pushing to giyhub
+// 	from := utils.Config.FromEmailAddr
+// 	password := os.Getenv("SMTPpwd")
+// 	toEmail := req.EmailAddress
+// 	to := []string{toEmail}
+// 	host := "smtp.gmail.com"
+// 	port := "587"
+// 	address := host + ":" + port
+// 	subject := req.EmailSubject
+// 	body := req.EmailBody
+// 	message := []byte(subject + "\n" + body)
+// 	auth := smtp.PlainAuth("", from, password, host)
+// 	err := smtp.SendMail(address, auth, from, to, message)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
+
 func NewEmailSrv(fromEmail string, password string, host string, port string) EmailService {
 	return &emailSrv{FromEmail: fromEmail, Password: password, Host: host, Port: port}
-}
-
-func SendMail(req emailEntity.SendEmailReq) error {
-
-	from := os.Getenv("FromEmailAddr")
-	password := os.Getenv("SMTPpwd")
-	toEmail := req.EmailAddress
-	to := []string{toEmail}
-	host := "smtp.gmail.com"
-	port := "567"
-	address := host + ":" + port
-	subject := req.EmailSubject
-	body := req.EmailBody
-	message := []byte(subject + body)
-	auth := smtp.PlainAuth("", from, password, host)
-	err := smtp.SendMail(address, auth, from, to, message)
-	if err != nil {
-		return err
-	}
-	return nil
 }
