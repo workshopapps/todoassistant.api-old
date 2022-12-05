ee9aa782bef01706f23c1399dc41832e30f190ca
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-12-05 12:58:19 +0100

    fix: Added access-control-allow-method to cors

--- cmd/middlewares/corsMiddleware.go
+++ cmd/middlewares/corsMiddleware.go
@@ -1,8 +1,9 @@
 package middlewares
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
@@ -10,6 +11,7 @@ func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
