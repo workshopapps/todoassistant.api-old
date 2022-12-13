848b2a5ecd814b067bdf32cccdf59780d586e270
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-12-13 10:46:44 +0100

    Addeail service to subscribe

--- internals/service/subscribeService/srv.go
+++ internals/service/subscribeService/srv.go
@@ -5,38 +5,63 @@ import (
 	"log"
 	"test-va/internals/Repository/subscribeRepo"
 	"test-va/internals/entity/ResponseEntity"
+	"test-va/internals/entity/emailEntity"
 	"test-va/internals/entity/subscribeEntity"
+	"test-va/internals/service/emailService"
 	"time"
 )
 
-type SubscribeService interface{
-	PersistEmail(req *subscribeEntity.SubscribeReq)(*subscribeEntity.SubscribeRes, *ResponseEntity.ServiceError)
+type SubscribeService interface {
+	PersistEmail(req *subscribeEntity.SubscribeReq) (*subscribeEntity.SubscribeRes, *ResponseEntity.ServiceError)
 }
 
-type subscribeSrv struct{
-	repo 	subscribeRepo.SubscribeRepository
-}
-func NewSubscribeSrv(repo subscribeRepo.SubscribeRepository) SubscribeService{
-	return &subscribeSrv{repo: repo}
+type subscribeSrv struct {
+	repo     subscribeRepo.SubscribeRepository
+	emailSrv emailService.EmailService
 }
 
-func (t *subscribeSrv) PersistEmail(req *subscribeEntity.SubscribeReq) (*subscribeEntity.SubscribeRes, *ResponseEntity.ServiceError){
+func NewSubscribeSrv(repo subscribeRepo.SubscribeRepository, emailSrv emailService.EmailService) SubscribeService {
+	return &subscribeSrv{repo: repo, emailSrv: emailSrv}
+}
+
+func (t *subscribeSrv) PersistEmail(req *subscribeEntity.SubscribeReq) (*subscribeEntity.SubscribeRes, *ResponseEntity.ServiceError) {
+	var message emailEntity.SendEmailReq
+
 	// create context of 1 minute
 	ctx, cancelFunc := context.WithTimeout(context.TODO(), time.Minute*1)
 	defer cancelFunc()
 
 	result, err1 := t.repo.CheckEmail(ctx, req)
-	if result != nil{
-		return nil, ResponseEntity.NewCustomServiceError("already subscribed", err1)
+	if result != nil {
+		return nil, ResponseEntity.NewCustomServiceError("Already subscribed", err1)
 	}
-	err := t.repo.PersistEmail(ctx, req)
+
+	message.EmailAddress = req.Email
+	message.EmailSubject = "Subject: Subscription To Ticked Newsletter\n"
+	message.EmailBody = CreateMessageBody()
+
+	err := t.emailSrv.SendMail(message)
 	if err != nil {
-		log.Println("From subcribe ",err)
 		return nil, ResponseEntity.NewInternalServiceError(err)
 	}
-	data:= subscribeEntity.SubscribeRes{
+
+	err = t.repo.PersistEmail(ctx, req)
+	if err != nil {
+		log.Println("From subcribe ", err)
+		return nil, ResponseEntity.NewInternalServiceError(err)
+	}
+	data := subscribeEntity.SubscribeRes{
 		Email: req.Email,
 	}
 
 	return &data, nil
+}
+
+// Auxillary function
+func CreateMessageBody() string {
+	subject := "Subscription to Ticked!\n\n"
+	mainBody := "Thank you for subscribing to our newsletter!\n\nGet ready for an awesome ride"
+
+	message := subject + mainBody
+	return string(message)
 }
